a5596c106417d8ede06b6d93faa071614ee191c2
yuyenews <yuyemail@163.com>
2021-10-14 13:14:10 +0800

    init commit

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,5 @@
-const Migrations = artifacts.require("Migrations");
 const UniswapV2Factory = artifacts.require("UniswapV2Factory");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
   deployer.deploy(UniswapV2Factory);
 };
